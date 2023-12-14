21f74dd51ec1b07aa1518e9691562452d0539078
Fox123582 <foxmax.od.ua@gmail.com>
2023-12-14 20:35:52 +0200

    Add

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/posts',checkAuth, postCreateValidation,handleValidationErrors, PostCo
 app.delete('/posts/:id',checkAuth,PostController.remove)
 app.patch('/posts/:id', checkAuth,postCreateValidation,handleValidationErrors,PostController.update)
 
-app.listen(4444,(err)=>{
+app.listen(process.env.PORT || 4444,(err)=>{
     if (err){
         return console.log(err)
     }
