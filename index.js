211514825057c7ff00cbdc0af30d89216bc2274c
Fox123582 <foxmax.od.ua@gmail.com>
2023-12-14 20:40:33 +0200

    Add

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import {loginValidation, postCreateValidation, registerValidation} from "./valid
 
 
 mongoose.connect(process.env.MONGODB_URL)
-
     .then(()=>console.log('DB ok'))
     .catch((err)=>console.log('DB error',err))
 
